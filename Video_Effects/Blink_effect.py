0c2997919b28f53dfc6df35478d6c8d712ac0e5d
jamonator <83436730+jamonator@users.noreply.github.com>
2024-03-19 20:39:36 +1100

    Update Blink_effect.py

--- Video_Effects/Blink_effect.py
+++ Video_Effects/Blink_effect.py
@@ -1,6 +1,6 @@
 from moviepy.editor import VideoFileClip, concatenate_videoclips, ColorClip
 
-def add_blinking_effect_to_clip(clip, d_on=0.1, d_off=0.1):
+def add_blinking_effect(clip, d_on=0.1, d_off=0.1):
     """Apply blinking effect to a video clip."""
     def black_clip(duration, fps):
         return ColorClip(size=(clip.size[0], clip.size[1]), color=(0, 0, 0), duration=duration).set_fps(fps)
@@ -8,7 +8,8 @@ def add_blinking_effect_to_clip(clip, d_on=0.1, d_off=0.1):
     blinking_clips = []
     t = 0
     while t < clip.duration:
-        blinking_clips.append(clip.subclip(t, t + d_on))
+        subclip_end = min(t + d_on, clip.duration)
+        blinking_clips.append(clip.subclip(t, subclip_end))
         blinking_clips.append(black_clip(d_off, clip.fps))  # Corrected FPS parameter
         t += d_on + d_off
 
